a62893d45daece4dfbff5573c33487df11c2bfa8
Trevor Xander <trevorcolexander@gmail.com>
2019-01-22 22:37:40 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
 
 setup(
     name='2048',
     version='0.1',
-    url='https://github.com/trevorxander/2048',
-    license='',
+    url='https://trevorxander.com/#2048',
+    license='GNU Affero General Public License v3.0',
     author='Trevor Xander',
     author_email='trevorcolexander@gmail.com',
-    description='',
-    install_requires = ['PyQt5', 'anytree','setuptools'],
+    long_description=read('README'),
+    install_requires=['PyQt5', 'anytree', 'setuptools'],
     py_modules=['2048'],
     packages=find_packages(),
     entry_points={
